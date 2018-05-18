9b1c46c0a8b0c404490916efba93379ed33c34d8
alexandresebrao <alexandre.sebrao@gmail.com>
2018-05-18 10:54:38 -0300

    CA-008 Renaming files to js as it is easier to read on coverage, more testing being made

---
+++ test/View/Mols/Header.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "../../../src/View/Mols/Header";
+import { shallow } from "enzyme";
+import { expect } from "chai";
+
+describe("Header rendering test", function() {
+    let wrapper;
+
+    before(function() {
+        wrapper = shallow(<Header />);
+    });
+
+    after(function() {
+        wrapper.unmount();
+    });
+
+    it("should render", function() {
+        expect(wrapper).to.have.length(1);
+    });
+
+    it("should render an img element", function() {
+        expect(wrapper.find("img")).to.have.length(1);
+    });
+});
